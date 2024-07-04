300236c36bd0dc862a7e4e0bac8ac829c2174161
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-07-04 04:12:01 +0200

    help-desk

--- depurar_usuarios.py
+++ depurar_usuarios.py
@@ -7,9 +7,14 @@ import time
 import shutil
 import subprocess
 import ctypes
-from colorama import init, Fore, Style
 
-init()
+
+amarillo = "\033[93m"
+azul     = "\033[94m"
+rojo     = "\033[91m"
+verde    = "\033[92m"
+blanco   = "\033[0m"
+
 
 def obtener_usuarios():
    directorio_de_usuarios = r"C:\Users"
@@ -22,25 +27,25 @@ def eliminar_usuarios(lista_usuarios):
    for usuario in lista_usuarios:
       try:
          # Eliminar usuario
-         subprocess.run(['powershell.exe', f'net user {usuario} /delete'], check=True)
-         print(Fore.GREEN + Style.BRIGHT + f"Usuario eliminado: [{usuario}]" + Style.RESET_ALL)
+         subprocess.Popen(['powershell.exe', f'net user {usuario} /delete'], creationflags=subprocess.CREATE_NO_WINDOW).wait()
          # Eliminar carpeta del usuario
          shutil.rmtree(os.path.join(r"C:\Users", usuario), ignore_errors=True)
          # Eliminar registros del usuario
-         subprocess.run(['powershell.exe', f'Remove-Item -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList\\*" -Recurse -Force -ErrorAction SilentlyContinue -Filter "*{usuario}"'], check=True)
+         subprocess.Popen(['powershell.exe', f'Remove-Item -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList\\*" -Recurse -Force -ErrorAction SilentlyContinue -Filter "*{usuario}"'], creationflags=subprocess.CREATE_NO_WINDOW).wait()
+         print(verde + f"Usuario: [{usuario}] eliminado." + blanco)
       except Exception as e:
-         print(Fore.RED + Style.BRIGHT + f"Error al intentar eliminar el usuario: [{usuario}]: {e}" + Style.RESET_ALL)
-   print(Fore.GREEN + Style.BRIGHT + f"Registros de usuarios residuales eliminados." + Style.RESET_ALL)
+         print(rojo + f"Error al intentar eliminar el usuario: [{usuario}]: {e}" + blanco)
+   print(verde + f"Registros residuales de usuarios eliminados." + blanco)
 
 
 def firma_autor():
    print("******************************************")
-   print(Fore.YELLOW + Style.BRIGHT + "MIJITO SE HAN DEPURADO LOS USUARIOS!\nAutor: @josuerom" + Style.RESET_ALL)
+   print(amarillo + "MIJITO SE HAN DEPURADO LOS USUARIOS!\nAutor: @josuerom" + blanco)
    print("******************************************\n")
 
 
 def reiniciar_sistema():
-   print(Fore.CYAN + Style.BRIGHT + "El sistema se reiniciará en 10 segundos..." + Style.RESET_ALL)
+   print(azul + "El sistema se reiniciará en muy pocos segundos..." + blanco)
    time.sleep(10)
    os.system("shutdown /r /f")
 
@@ -59,6 +64,6 @@ if __name__ == "__main__":
       firma_autor()
       reiniciar_sistema()
    else:
-      print(Fore.RED + Style.BRIGHT + "¡ANIMAL! Ejecuta este programa desde el usuario [ .\soporte ]" + Style.RESET_ALL)
-      print("¡Ingeniero! presione cualquier tecla para salir...")
+      print(rojo + "¡ANIMAL! Ejecuta este programa desde el usuario: [ soporte ]" + blanco)
+      print(azul + "Presione cualquier tecla para salir..." + blanco)
       input()
