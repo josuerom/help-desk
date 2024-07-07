6648762b10a37c1f954b6a96b8bdf88fc3fc926d
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-07-07 15:52:14 -0500

    help-desk

---
+++ scripts/Alojar_Clave_Producto.ps1
@@ -0,0 +1,24 @@
+$key = (wmic path SoftwareLicensingService get OA3xOriginalProductKey).Split("`n")[2].Trim()
+Write-Output "LICENCIA ALOJADA DEL PRODUCTO: [ $key ]`n"
+if ($key -ne "") {
+   Write-Output "Eliminando la clave de producto actual...`n"
+   cscript //B //NoLogo "%windir%\System32\slmgr.vbs" /upk
+   Write-Output "Clave de producto eliminada.`n"
+} else {
+   Write-Output "No se encontró una clave de producto para eliminar.`n"
+}
+do {
+   $newKey = Read-Host "Ingrese la nueva clave de producto >>"
+   if ($newKey -eq "") {
+      Write-Output "Debe ingresar una clave de producto valida.`n"
+   }
+} while ($newKey -eq "")
+cscript //B //NoLogo "%windir%\System32\slmgr.vbs" /ipk $newKey
+Write-Output "`nClave de producto insertada.`n"
+Write-Output "Activando la nueva clave de producto...`n"
+cscript //B //NoLogo "%windir%\System32\slmgr.vbs" /ato
+Write-Output "----------------------------------"
+Write-Output "::  CLAVE DE PRODUCTO ACTIVADA  ::"
+Write-Output "----------------------------------"
+Write-Output "`nAutor: @josuerom"
+Pause
