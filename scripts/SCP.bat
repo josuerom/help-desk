f2855c4c87ecfbb901ad59bf6628a8bcfdd7ddfe
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-07-19 22:22:32 -0500

    Protocolo seguro de copias.bat

--- scripts/SCP.bat
+++ scripts/SCP.bat
@@ -2,24 +2,18 @@
 setlocal
 title SCP -- Atento Telares -- JR3
 
-color 0A
+color 2
 set /p ip_servidor="IP SERVIDOR: "
-set /p puerto_servidor="PUERTO SERVIDOR: "
+set /p puerto_ssh="PUERTO SERVIDOR: "
 set /p usuario_servidor="USUARIO SERVIDOR: "
-set /p origen_servidor="ORIGEN DEL SERVIDOR [/c/Herramientas/a.bat]: "
+set /p origen_servidor="ORIGEN SERVIDOR [c:\apps\act.bat]: "
 set /p usuario_destino="USUARIO DESTINO: "
 set destino=C:\Users\%usuario_destino%\Desktop
 
-REM set /p origen_servidor="ORIGEN SERVIDOR [/c/Bin/A.bat]: "
-REM set /p usuario_destino="USUARIO DESTINO LOCAL: "
-REM set ip_servidor=172.28.144.137
-REM set puerto_servidor=3003
-REM set usuario_servidor=josue.romero
-REM set destino=C:\Users\%usuario_destino%\Desktop
+echo.
+scp -P %puerto_ssh% %origen_servidor% %usuario_servidor%@%ip_servidor%:%destino%
+color 3
 
 echo.
-scp -P %puerto_servidor% %usuario_servidor%@%ip_servidor%:%origen_servidor% "%destino%"
-echo.
-
-color 07
+color 7
 pause
