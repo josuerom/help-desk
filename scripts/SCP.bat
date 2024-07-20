00a9017e32a87f2a31c116b612570bf4f99208dc
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-07-19 21:15:34 -0500

    SCP.bat

--- scripts/SCP.bat
+++ scripts/SCP.bat
@@ -3,19 +3,19 @@ setlocal
 title (SCP | Atento Telares | JR3)
 
 color 0A
-set /p ip_servidor="IP SERVIDOR -> "
-set /p puerto_servidor="PUERTO SERVIDOR -> "
-set /p usuario_servidor="USUARIO SERVIDOR -> "
-set /p origen_servidor="ORIGEN DEL SERVIDOR [/c/Herramientas/abc.bat] -> "
-set /p usuario_destino="USUARIO DESTINO -> "
+set /p ip_servidor="IP SERVIDOR: "
+set /p puerto_servidor="PUERTO SERVIDOR: "
+set /p usuario_servidor="USUARIO SERVIDOR: "
+set /p origen_servidor="ORIGEN DEL SERVIDOR [/c/Herramientas/a.bat]: "
+set /p usuario_destino="USUARIO DESTINO: "
 set destino=C:\Users\%usuario_destino%\Desktop
 
-REM set /p origen_servidor="ORIGEN SERVIDOR [/c/Bin/Push.bat]: "
-REM set /p usuario_destino="USUARIO DE DESTINO: "
+REM set /p origen_servidor="ORIGEN SERVIDOR [/c/Bin/A.bat]: "
+REM set /p usuario_destino="USUARIO DESTINO LOCAL: "
 REM set ip_servidor=172.28.144.137
-REM set puerto_servidor=2002
+REM set puerto_servidor=3003
 REM set usuario_servidor=josue.romero
-REM set destino=C:\Users\%username%\Desktop
+REM set destino=C:\Users\%usuario_destino%\Desktop
 
 echo.
 scp -P %puerto_servidor% %usuario_servidor%@%ip_servidor%:%origen_servidor% "%destino%"
