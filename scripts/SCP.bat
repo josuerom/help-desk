724f3c1353832608add6df7bbc6065d8cdd17421
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-07-20 16:52:26 -0500

    SCP.bat

--- scripts/SCP.bat
+++ scripts/SCP.bat
@@ -1,19 +1,30 @@
 @echo off
 setlocal
 title SCP -- Atento Telares -- JR3
+color 6
 
-color 2
-set /p ip_servidor="IP SERVIDOR: "
-set /p puerto_ssh="PUERTO SERVIDOR: "
-set /p usuario_servidor="USUARIO SERVIDOR: "
+set /p puerto_servidor="PUERTO: "
 set /p origen_servidor="ORIGEN SERVIDOR: "
+set /p usuario_servidor="USUARIO SERVIDOR: "
+set /p ip_hostname_servidor="IP-HOST SERVIDOR: "
 set /p usuario_destino="USUARIO DESTINO: "
-set destino="c:\users\%usuario_destino%\desktop"
+set destino=c:\users\%usuario_destino%\desktop
 
 echo.
 color 3
-scp -P %puerto_ssh% "%origen_servidor%" %usuario_servidor%@%ip_servidor%:%destino%
 
-echo.
-color 7
-pause
+scp -P %puerto_servidor% %usuario_servidor%@%ip_hostname_servidor%:"%origen_servidor%" "%destino%"
+
+if %errorlevel% equ 0 (
+    color 2
+    echo.
+    echo EXITO. La transferencia fue exitosa.
+    echo.
+    pause
+) else (
+    color 4
+    echo.
+    echo ERROR. Al intentar la transferencia animal.
+    echo.
+    pause
+)
