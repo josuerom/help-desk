cbd2d2c2f0a4b56545eacdbf90e1f01bc117ef85
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-07-21 00:40:41 +0200

    Facil_SCP.bat

--- scripts/Facil_SCP.bat
+++ scripts/Facil_SCP.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal
-title FSCP :: Atento Telares :: JR3
+title Copiado Facil y Seguro :: Atento Telares
 color 6
 
 set /p puerto_servidor="PUERTO: "
 set /p usuario_servidor="USUARIO SERVIDOR: "
 set /p ip_servidor="IP SERVIDOR: "
 set /p origen_servidor="ORIGEN SERVIDOR: "
-set /p destino_cliente="DESTINO CLIENTE: "
+set /p destino_cliente="DESTINO LOCAL: "
 
 echo.
 scp -P %puerto_servidor% %usuario_servidor%@%ip_servidor%:%origen_servidor% %destino_cliente%
@@ -15,13 +15,13 @@ scp -P %puerto_servidor% %usuario_servidor%@%ip_servidor%:%origen_servidor% %des
 if %errorlevel% equ 0 (
     color 2
     echo.
-    echo EXITO. La transferencia fue exitosa.
-    echo.
-    pause
+    echo La transferencia fue exitosa.
 ) else (
     color 4
     echo.
-    echo ERROR. Al intentar la transferencia animal.
-    echo.
-    pause
+    echo Error al intentar la conexion ANIMAL.
 )
+
+echo Autor: @josuerom
+echo.
+pause
