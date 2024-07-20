28b41c2e7c75382b2f6928493675b954eab078e3
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-07-21 00:13:10 +0200

    Update SCP.bat

--- scripts/SCP.bat
+++ scripts/SCP.bat
@@ -4,16 +4,13 @@ title SCP -- Atento Telares -- JR3
 color 6
 
 set /p puerto_servidor="PUERTO: "
-set /p origen_servidor="ORIGEN SERVIDOR: "
 set /p usuario_servidor="USUARIO SERVIDOR: "
-set /p ip_hostname_servidor="IP-HOST SERVIDOR: "
-set /p usuario_destino="USUARIO DESTINO: "
-set destino=c:\users\%usuario_destino%\desktop
+set /p ip_servidor="IP-HOST SERVIDOR: "
+set /p origen_servidor="ORIGEN SERVIDOR: "
+set /p destino_cliente="DESTINO CLIENTE: "
 
 echo.
-color 3
-
-scp -P %puerto_servidor% %usuario_servidor%@%ip_hostname_servidor%:"%origen_servidor%" "%destino%"
+scp -P %puerto_servidor% %usuario_servidor%@%ip_servidor%:%origen_servidor% %destino_cliente%
 
 if %errorlevel% equ 0 (
     color 2
