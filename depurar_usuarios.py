b2027d7b54d804b179f182fdfac2c33155fc4986
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-07-20 19:56:16 -0500

    Actualizacion 1.4

--- depurar_usuarios.py
+++ depurar_usuarios.py
@@ -1,6 +1,6 @@
 """
 PyScript para la depuración de usuarios automatica en Windows
-Autor: @josuerom Fecha: 16/Julio/2024
+Autor: @josuerom Fecha: 20/Julio/2024
 """
 import os
 import time
@@ -24,44 +24,65 @@ def color_print(color):
 
 def establecer_marca():
    ctypes.windll.kernel32.SetConsoleIcon(ctypes.windll.kernel32.LoadLibraryW(r".\icono.ico"))
-   ctypes.windll.kernel32.SetConsoleTitleW("DEPURADOR USER | ATENTO 2024 | JR3")
+   ctypes.windll.kernel32.SetConsoleTitleW("DEPURADOR DE USUARIOS | ATENTO COLOMBIA 2024 | TELARES")
 
 
-def eliminar_usuarios():
+def consultar_data_usuario() -> list:
    def obtener_usuarios():
       directorio_de_usuarios = r"C:\Users"
-      carpetas_excluidas = ["admin", "prestamo", "josue.romero" "soporte", "public", "all users", "default", "default user"]
+      carpetas_excluidas = [
+         "admin",
+         "prestamo",
+         "josue.romero",
+         "soporte",
+         "public",
+         "all users",
+         "default",
+         "default user"
+      ]
       usuarios = [carpeta for carpeta in os.listdir(directorio_de_usuarios) if os.path.isdir(os.path.join(directorio_de_usuarios, carpeta)) and carpeta.lower() not in [ex.lower() for ex in carpetas_excluidas]]
       return usuarios
 
-   lista_usuarios = obtener_usuarios()
-   contador, del_exitoso = 0, 0
    host = socket.gethostname()
+   lista_usuarios = obtener_usuarios()
+   contador = 0
 
    color_print(amarrillo)
-   print(f"A CONTINUACION SE INTENTARAN ELIMINAR ({len(lista_usuarios)}) RESIDUOS DE USUARIOS DEL {host}.\n")
+   print(f"A CONTINUACION, SE RECOPILARAN ({len(lista_usuarios)}) DATAS DE USUARIOS DEL HOST: '{host}'.\n")
+
+   lsr_definitiva = []
 
    for usuario in lista_usuarios:
       try:
          contador += 1
-         cmd_dirusuario = fr"Remove-Item -Path C:\Users\{usuario} -Recurse -Force"
-         estado_uno = subprocess.Popen(['powershell.exe', '-Command', cmd_dirusuario], creationflags=subprocess.CREATE_NO_WINDOW).wait()
-         comando = fr'(Get-WmiObject Win32_UserProfile | Where-Object {{ $_.LocalPath -like "*\{usuario}" }}).SID'
+         lsr_definitiva.append(fr"Remove-Item -Path C:\Users\{usuario} -Recurse -Force")
+
+         comando = fr'(Get-WmiObject Win32_UserProfile | Where-Object {{ $_.LocalPath -like "*\grcorreacwer" }}).SID'
          sid_process = subprocess.Popen(['powershell.exe', '-Command', comando], stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
+
          stdout, stderr = sid_process.communicate()
+
          SID_usuario = stdout.strip().decode('utf-8')
-         cmd_definitivo = f"Remove-Item -Path HKLM:\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList\\{SID_usuario} -Recurse"
-         estado_dos = subprocess.Popen(['powershell.exe', '-Command', cmd_definitivo], creationflags=subprocess.CREATE_NO_WINDOW).wait()
-         if estado_uno == 0 and estado_dos == 0:
-            del_exitoso += 1
-         color_print(verde)
-         print(f"Usuario {contador}. [{usuario}]\t\t--> ELIMINADO.")
+         if SID_usuario != False:
+            lsr_definitiva.append(fr"Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\{SID_usuario}' -Recurse -Force")
+
+         print(f"Usuario {contador} -> [{usuario}]\tCONSULTADO.")
       except Exception as e:
          color_print(rojo)
-         print(f"Usuario {contador}: [{usuario}]\t\t--> NO ELIMINADO.")
+         print(f"Usuario {contador} -> [{usuario}]\tNO OBTENIDO.")
 
    color_print(verde)
-   print(f"\nSE ALCANZARON A ELIMINAR ({del_exitoso}) USUARIOS COMPLETAMENTE.\n")
+   print("SE PROCEDE A CONSTRUIR EL 'C:\\Users\\Public\\Downloads\\dpu.ps1'\n")
+   return lsr_definitiva
+
+
+def contruir_programa_ps1(lsr_definitiva):
+   ruta_archivo_ps1 = r'C:\Users\Public\Downloads\dpu.ps1'
+   with open(ruta_archivo_ps1, 'w') as contenido_archivo_ps1:
+      for consulta in lsr_definitiva:
+         contenido_archivo_ps1.write(consulta + "\n")
+
+   ejecutar_programa(ruta_archivo_ps1)
 
 
 def firma_autor():
@@ -70,20 +91,26 @@ def firma_autor():
    color_print(blanco)
    print("**************************************")
    color_print(amarrillo)
-   print(f"MIJITO SE HAN DEPURADO LOS USUARIOS!\nFecha actual: {fecha}\n\nAUTOR: @josuerom")
+   print(f"MIJITO/A SE HAN DEPURADO LOS USUARIOS!\nFecha de ejecucion: {fecha}\n\nAUTOR: @josuerom")
    color_print(blanco)
    print("**************************************\n")
 
 
+def ejecutar_programa(ubicacion_programa):
+   try:
+      subprocess.run(['powershell.exe', '-File', ubicacion_programa], capture_output=False, text=True, check=True)
+   except subprocess.CalledProcessError as e:
+      print(f'ERROR:\n{e.stderr}')
+
+
 def cerrar_sesion():
-   color_print(azul)
-   print("Se cerrara la sesion en pocos segundos...")
    time.sleep(7)
    os.system("shutdown /l /f")
 
 
 if __name__ == "__main__":
    establecer_marca()
-   eliminar_usuarios()
+   lista_resultante_definitiva = consultar_data_usuario()
+   contruir_programa_ps1(lista_resultante_definitiva)
    firma_autor()
    cerrar_sesion()
--- scripts/Facil_SCP.bat
+++ scripts/Facil_SCP.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBpQQQ2MAFuzBaEJ+u3o0+OErUMSGus8d+8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlXi
+::ZQ05rAF9IAHYFVzEqQIWi9pXRQjCD0iKZg==
+::eg0/rx1wNQPfEVWB+kM9LVsJDAKDP2K2FbYMiA==
+::fBEirQZwNQPfEVWB+kM9LVsJDAKDP2K2FbYMiA==
+::cRolqwZ3JBvQF1fEqQIDCysUUESjKG60Erpc3O337viCsQ0BFMsxf8Gb8LudNeVT2ErpcIQitg==
+::dhA7uBVwLU+EWHiQ8UwkJ1t3QwiNMWmzB9U=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE2WsEaEkaHGQ=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJHaU4Es9PQgUfBaLMW6GVuMjzun45ua0gX1TUfo6GA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 setlocal
 title Copiado Facil y Seguro :: Atento Telares
@@ -7,19 +40,18 @@ set /p puerto_servidor="PUERTO: "
 set /p usuario_servidor="USUARIO SERVIDOR: "
 set /p ip_servidor="IP SERVIDOR: "
 set /p origen_servidor="ORIGEN SERVIDOR: "
-set /p destino_cliente="DESTINO LOCAL: "
+set /p destino_local="DESTINO LOCAL: "
 
-echo.
-scp -P %puerto_servidor% %usuario_servidor%@%ip_servidor%:%origen_servidor% %destino_cliente%
+scp -P %puerto_servidor% %usuario_servidor%@%ip_servidor%:%origen_servidor% %destino_local%
 
 if %errorlevel% equ 0 (
     color 2
     echo.
-    echo La transferencia fue exitosa.
+    echo La transferencia de informacion fue exitosa.
 ) else (
     color 4
     echo.
-    echo Error al intentar la conexion ANIMAL.
+    echo OJO: Hubo un error al intentar la conexion.
 )
 
 echo Autor: @josuerom
